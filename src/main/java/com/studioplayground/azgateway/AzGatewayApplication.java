19f47817233123668851a7602137e2011d262986
jiho <tlswlgh120@naver.com>
2023-04-18 21:31:04 +0900

    :tada: Initial Commit

---
+++ src/main/java/com/studioplayground/azgateway/AzGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.studioplayground.azgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studioplayground/azgateway/AzGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studioplayground.azgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
